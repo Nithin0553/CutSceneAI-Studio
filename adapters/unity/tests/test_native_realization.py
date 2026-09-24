93571a31011b42ebd89194e2ec18e3e68c05bdd0
Nithin Thirumani <nithinthirumani0553@gmail.com>
2026-09-24 02:35:01 -0500

    unity tests: match stream binding call syntax

--- adapters/unity/tests/test_native_realization.py
+++ adapters/unity/tests/test_native_realization.py
@@ -175,7 +175,7 @@ def test_compiles_exact_bundle_and_renders_strict_unity_editor_harness() -> None
         "realized_right_knee_local_delta_deg",
         "animation_stream_probe_left_knee_delta_deg",
         "animation_stream_probe_right_knee_delta_deg",
-        "AnimatorJobExtensions.BindStreamTransform",
+        "animator.BindStreamTransform",
         "AnimationScriptPlayable.Create",
         "SetLocalRotation(stream",
         "realized_left_ankle_direction_from_knee",
